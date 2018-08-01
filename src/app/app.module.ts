c99c1a48e01e389797ad096e60f04c2ba9cf4804
Denis Pupyrev <adaptmen@gmail.com>
2018-08-01 12:03:32 +0400

    Добавление Angular _1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AuthModule } from '../auth/auth.module';
+import { AuthModule } from './auth/auth.module';
 
 import { AppComponent } from './app.component';
 

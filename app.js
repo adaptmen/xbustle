f34168700c206ee5f59a8136499b72da0e63164e
Denis Pupyrev <adaptmen@gmail.com>
2018-08-03 01:50:17 +0400

    Настройка роутинга в главном файле + middleware для cookies

--- app.js
+++ app.js
@@ -11,10 +11,6 @@ var api_controller = require('./controllers/apiController');
 
 var app = express();
 
-app.set('views', path.join(__dirname, 'views'));
-app.engine('ejs', require('ejs-locals'));
-app.set('view engine', 'ejs');
-
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
@@ -22,39 +18,13 @@ app.use(bodyParser.urlencoded({
 }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'dist/xbustle')));
-app.use(session({
-    secret: 'tkt',
-    resave: false,
-    saveUninitialized: true,
-    cookie: {
-        maxAge: 60000000
-    }
-}))
 
-app.get('/', function (req, res) {
+
+app.all('*', function (req, res) {
     res.sendFile(path.join(__dirname, 'dist/xbustle/index.html'))
 });
 
-app.all('/api/*', api_controller);
-
-app.use(function (req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
-});
-
-// error handler
-app.use(function (err, req, res, next) {
-    // set locals, only providing error in development
-    res.locals.message = err.message;
-    res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-    // render the error page
-    res.status(err.status || 500);
-    res.render('error', {
-        err: err
-    });
-});
+app.use('/api/*', api_controller);
 
 app.disable('etag');
 

8e3a0b7cabe36750bbcc357bbe9bdc7859968842
Denis Pupyrev <adaptmen@gmail.com>
2018-08-04 23:35:22 +0400

    Избавление от лишнего в api-контроллере

--- controllers/apiController.js
+++ controllers/apiController.js
@@ -8,13 +8,11 @@ var urlencodedParser = bodyParser.urlencoded({
 });
 var db = require("../core/db")();
 
-var login_controller = require('./controllers/loginController');
-var signup_controller = require('./controllers/signupController');
 var user_controller = require('./userController');
 var task_controller = require('./taskController');
 
-router.all('user/*', user_controller);
-router.all('task/*', task_controller);
+router.all('/api/user/*', user_controller);
+router.all('/api/task/*', task_controller);
 
 router.get('/logout', function (req, res) {
     let cookie = new Cookies(req, res);

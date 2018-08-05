1721a9a4d2d80e6ef5ee3cad4b7840030ca69556
Denis Pupyrev <adaptmen@gmail.com>
2018-08-06 02:41:26 +0400

    Защита роута "/user"

--- src/app/shared/services/auth.service.ts
+++ src/app/shared/services/auth.service.ts
@@ -1,13 +1,33 @@
 import { Http } from '@angular/http';
 import { Injectable } from '@angular/core';
+import { 
+  ActivatedRouteSnapshot,
+  CanActivate,
+  RouterStateSnapshot
+} from '@angular/router';
 import { Observable, pipe } from 'rxjs';
 import { map } from 'rxjs/operators';
 
 
 @Injectable()
-export class AuthService {
+export class AuthService implements CanActivate {
   
   constructor(private http: Http) {}
+  
+  private isLogin;
+  
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): any {
+    this.isLogged().then((result) => {
+      if (result.status_code == "user_found") {
+        this.isLogin = true;
+        return true;
+      }
+      else {
+        this.isLogin = false;
+        return false;
+      }
+    });
+  }
   
   login(formData: object): Observable<any> {
     return this.http.post('http://localhost:3000/api/user/login', { user: formData })
@@ -17,6 +37,13 @@ export class AuthService {
   signup(formData: object): Observable<any> {
      return this.http.post('http://localhost:3000/api/user/signup', { user: formData })
     .pipe(map((res) => {return res.json()}));
+  }
+  
+  isLogged() {
+    return new Promise((resolve, reject) => { 
+      this.http.get('http://localhost:3000/api/user/islogged')
+      .pipe(map(res => res.json())).subscribe(resolve, reject);
+    })
   }
   
 }

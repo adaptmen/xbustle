6e03e0ea2c19a51b6ac2302c23cbe1ac75741d83
Denis Pupyrev <adaptmen@gmail.com>
2018-08-10 15:36:25 +0400

    Server. Определение текущего пользователя при обращении к api

--- controllers/apiController.js
+++ controllers/apiController.js
@@ -4,16 +4,47 @@ var session = require('express-session');
 var Cookies = require('cookies');
 var bodyParser = require("body-parser");
 var urlencodedParser = bodyParser.urlencoded({
-    extended: false
+  extended: false
 });
 var db = require("../core/db")();
+var userService = require("../core/user-service");
+var sendy = require("../core/sendy/sendy");
 
 var user_controller = require('./userController');
 var tasks_controller = require('./tasksController');
 var team_controller = require('./teamController');
 
+router.all('*', (req, res, next) => {
+  let cookie = new Cookies(req, res);
+  if (cookie.get('token') === "") {
+    next()
+  } else if (userService.currentUser !== {}) {
+    db
+      .table('users')
+      .where({
+        token: cookie.get('token')
+      })
+      .get()
+      .then(
+        (result) => {
+          result !== [] ?
+            (() => {
+              let cookies = new Cookies(req, res);
+              userService.currentUser = result[0];
+              cookies.set('token', result[0].token);
+              next();
+            })() :
+            res.send(sendy("user_not_found"));
+        },
+        (error) => {
+          res.send(sendy("db_connection_error", error));
+        }
+      )
+  }
+});
+
 router.all('/api/user/*', user_controller);
-router.all('/api/task/*', tasks_controller);
+router.all('/api/tasks/*', tasks_controller);
 router.all('/api/team/*', team_controller);
 
 module.exports = router;

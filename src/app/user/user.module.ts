d59b1cbf54e477b5eaed9ac2f0bcf329b950b257
Denis Pupyrev <adaptmen@gmail.com>
2018-08-10 15:47:07 +0400

    Разделение роутов, разделение Front-компонентов

--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -1,19 +1,25 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { UserRoutingModule } from './user-routing.module';
+import { SharedModule } from '../shared/shared.module';
+import { ScrollbarModule } from 'ngx-scrollbar';
 
 import { UserComponent } from './user.component';
 import { TasksComponent } from './tasks/tasks.component';
+import { AddComponent } from './tasks/add/add.component';
 
 @NgModule({
   imports: [
     CommonModule,
     UserRoutingModule,
-    UserRoutingModule
+    UserRoutingModule,
+    ScrollbarModule,
+    SharedModule
   ],
   declarations: [
     UserComponent,
-    TasksComponent
+    TasksComponent,
+    AddComponent
   ]
 })
 export class UserModule { }
